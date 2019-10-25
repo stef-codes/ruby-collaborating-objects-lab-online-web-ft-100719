6f4afc0a2df7baa1ddcf94ae4575231a97b45021
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-25 07:09:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
   end 
   
   def artist_name=(artist_name)
-      Artist.find_or_create_by_name
+      Artist.find_or_create_by_name(artist_name)
   end 
   
   def self.new_by_filename(filename)
